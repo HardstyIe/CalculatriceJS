814f8985c01cf203f673df80b7cc7caf1b67063f
Hardstyle <Hardstyle.dd@gmail.com>
2023-05-12 15:52:27 +0200

    calculatrice

--- src/assets/JS/index.js
+++ src/assets/JS/index.js
@@ -1,8 +1,9 @@
 // DOM
-const touches = [...document.querySelectorAll(".bouton")];
+const touches = [...document.querySelectorAll(".buttonCalc")];
 const listeKeycode = touches.map((touche) => touche.dataset.key);
 const ecran = document.querySelector(".ecran");
 
+console.log(touches);
 document.addEventListener("keydown", (e) => {
   const valeur = e.keyCode.toString();
   calculer(valeur);
@@ -12,18 +13,24 @@ touches.forEach((touche) => {
   touche.addEventListener("click", (e) => {
     const valeur = e.target.dataset.key;
     calculer(valeur);
+    console.log(valeur);
   });
 });
+
+const boutonEffacer = document.querySelector("#bouton-effacer");
 
 const calculer = (valeur) => {
   if (listeKeycode.includes(valeur)) {
     switch (valeur) {
-      case "8":
+      case "49,3":
         ecran.textContent = "";
         break;
       case "13":
         const calcul = eval(ecran.textContent);
         ecran.textContent = calcul;
+        break;
+      case "8":
+        ecran.textContent = ecran.textContent.slice(0, -1);
         break;
       default:
         const indexKeycode = listeKeycode.indexOf(valeur);
